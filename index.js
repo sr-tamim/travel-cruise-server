7bc7c285166486b6bcec51872b077ecfaa72e48f
SR TAMIM <saifur.tamim@gmail.com>
2021-10-29 07:48:52 +0600

    server setup and heroku

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Travel Cruise server is ready.!'))
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
